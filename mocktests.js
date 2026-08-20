e409701e0c23bef5ad1d2332c34301f1e4f2ae68
raunakpc23-a11y <raunakpc23@gmail.com>
2026-08-20 18:45:39 +0530

    Update mocktests.js

--- mocktests.js
+++ mocktests.js
@@ -1,5 +1,153 @@
 // mocktests.js
 const mockTests = [
-  // You will add your mock test PDFs here later.
-  // Make sure their folders start with "SIMULATOR"
+  {
+    "title": "QFT 03 Solutions",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/10CKcPbZX8wYn1sCQ9Ejo-V6d4NtJ1sgt/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "QFT - 03",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1HFrdIYmQM8zvqbu22UHhOuCRA72eLkqL/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "QFT-2 Qs",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1EmdFY7LfjSG8QBaoKfBkd_T13GwcA2aw/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "QFT - 01 (ANS)",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/15A6m2Pi_KKcaevgznmZnZhSRGt6aLQF7/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "QFT - 01 JEE 26",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1eTtS2_4iQbvH_OqlhgcQPDaK9qr3KAUb/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "QPT 6",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/13KiD70IMviFMJq9prnfYu-mrpQFFyLOQ/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "qpt 06 sol",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1V4yOtI74FsZAayWXAVOOyak9_O-F3utf/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "qpt 07 sol",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/146Fp6O5NbgmdNky-VJxYfFwz7FLMOdZE/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "qpt 07",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/159mMZBiH_8P36b0elpe-Scyqa6Zn3mLt/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "qpt5_solution",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1-o-Hr8znfY9FFBPsm2Sq93AA7LRma-4q/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "qft 5",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1KVImjlcgKksWV36jDVSwc6YRW_qKV7e1/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "QPT 04 Solutions 2026",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1bEp_dwyADPsxE8IHcB4fNhz2abVKQhSJ/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "Mathongo paper 4",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1jCtkrJClJbHFqMOIeKaXDqjjgM8L-gVl/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "QPT - 03 Mathango 2026",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/14F7Z-HhW29hBtkIPXs3e1rBAzwB96E4I/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "qpt3_solution",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/18eG4ISmTC0f0n7zgktr1gcKOe8PpRHNc/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "Part Test 02 Mathango (Solutions)",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1vFDJC_mL-5iWARMhzD7iWPxjL-R1F5D3/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "mathongo paper 2",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1M4GcyjpigyTtVuHOIQPLntJwhCNIEc_W/preview",
+    "answerKeyUrl": ""
+  },
+  {
+    "title": "Full_Test-2",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1Yn2gSCKocapD7bDcfOfoicmKgAQ1D87m/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1y-jzjTEWe61LkuotrTB6FtR2F2PphIhi/preview"
+  },
+  {
+    "title": "Full_Test-4",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1Xc2iIm_e0Qv9nccMSPZIRnNeZZ_qGGe1/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1qV7xOENTUbdmeQoxxw7wiBQGuFolfXpH/preview"
+  },
+  {
+    "title": "Full_Test-3",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1pLELrs_UuaBLcLAtYHLm4cGqshY88sP2/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1T4ETO_pNlpndJbLNRfJ0S3_0uu3CA33S/preview"
+  },
+  {
+    "title": "Full_Test-5",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1y5f_fjICkSmCIIhDCdKl0H8n-1-OMtYA/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1xLQ_St0nX5_BgUg28ayFuo487eeon5wb/preview"
+  },
+  {
+    "title": "Full_Test-6",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1m0zwUnESJLy3J4NiFGAII5gLvU26RVC5/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1e64boBT_uWliCzDcS5y8dHWPNeS_2nre/preview"
+  },
+  {
+    "title": "Full_Test-7",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/14vw2ZZ1k3zYmLO5zX-wP4JGkDqbG7wFD/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1wk8fr3LyvJeuyrIHR46PWBbr5x-r8Hgi/preview"
+  },
+  {
+    "title": "Full_Test-8",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1u90Nsx2KUFd1PruB4-jx2FFoRQl0WOZ1/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1E0dsmmHALWK5rBBevUQCf8cMFsJPSfHi/preview"
+  },
+  {
+    "title": "Full_Test_1",
+    "folders": ["SIMULATOR", "Mathango Papers"],
+    "questionUrl": "https://drive.google.com/file/d/1WOmj7oH2IOvToZx-lM5N9JaOHC0B--iF/preview",
+    "answerKeyUrl": "https://drive.google.com/file/d/1HGfPQ1VKoslUukLtb6gjkaR5wF26u_r2/preview"
+  }
 ];
